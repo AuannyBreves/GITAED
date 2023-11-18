561970b5692f040413733c2eb6b7f7aaa9cfaa30
AuannyBreves <acbr.lic20@uea.edu.br>
2023-11-18 01:14:41 -0400

    correção do arquivo 20231109

--- 20231109/main.c
+++ 20231109/main.c
@@ -1,60 +1,59 @@
-#include "lista.c"
+#include <stdio.h>
+#include <time.h>
 
-int main(void){
-    int busca, pos, tamanho;
-    int *c;
-    
-    // CRIAR, INSERIR E IMPRIMIR LISTA1
-    printf("Qual o tamanho da sua lista? ");
-    scanf("%d", &tamanho);
-    lista * lista1 = criar_lista(tamanho);
-    inserir_elementos(lista1);
-    printf("IMPRIMINDO INTERATIVO");
-    imprimir_elementos(lista1);
-    printf("\nIMPRIMINDO RECURSIVO");
-    imprimir_elementos_recursivo(lista1, 0);
-    
-    // OPERAÇÕES LISTA1
-    printf("\nOPERAÇÕES: ------------------------------------------");
-    printf("\nMaior valor: %d", maior_recursivo(lista1, 0, 0));
-    printf("\nMenor valor: %d", menor_recursivo(lista1, 0, 1000));
-    printf("\nSoma: %d", soma_recursivo(lista1, 0));
-    printf("\nProduto: %d", produto_recursivo(lista1, 0));
-    
-    //DESTRUINDO LISTA1
-    destruir_lista(lista1);
-    printf("\n\nDESTRUINDO LISTA: ------------------------------------------\nLista1 liberada.");
-    
-    // CRIAR, INSERIR E IMPRIMIR LISTA2
-    lista * lista2 = criar_lista(30);
-    inserir_lista_ordenada(lista2);
-    printf("\n\nBUSCA L2: ------------------------------------------");
-    imprimir_elementos_recursivo(lista2, 0);
-
-     // BUSCA NÚMERO COM BINÁRIA RECURSIVA LISTA2
-    printf("\n\nQual valor existente deseja buscar? ");
-    scanf("%d", &busca);
-    
-    pos = busca_binaria_recursiva(lista2, busca, c, 0, lista2->quant-1);
-    if (pos != -1){
-        printf("COMPARAÇÕES: %d    BUSCA BINÁRIA RECURSIVA: %d\n", *c, pos);
+int maximo(int vetor[], int tamanho) {
+    if (tamanho == 1) {
+        return vetor[0];
+    }
+    int sub_max = maximo(vetor, tamanho - 1);
+    if (sub_max > vetor[tamanho - 1]) {
+        return sub_max;
     } else {
-        printf("COMPARAÇÕES: %d    BUSCA BINÁRIA RECURSIVA: Não encontrado.\n", *c);
+        return vetor[tamanho - 1];
+    }
+}
+
+int minimo(int vetor[], int tamanho) {
+    if (tamanho == 1) {
+        return vetor[0];
+    }
+    int sub_min = minimo(vetor, tamanho - 1);
+    if (sub_min < vetor[tamanho - 1]) {
+        return sub_min;
+    } else {
+        return vetor[tamanho - 1];
+    }
+}
+
+int soma(int vetor[], int tamanho) {
+    if (tamanho == 0) {
+        return 0;
+    }
+    return soma(vetor, tamanho - 1) + vetor[tamanho - 1];
+}
+
+int produto(int vetor[], int tamanho) {
+    if (tamanho == 0) {
+        return 1;
+    }
+    return produto(vetor, tamanho - 1) * vetor[tamanho - 1];
+}
+
+int main(){
+    int tamanho = 5;
+    int vetor[tamanho];
+    
+    //GERAÇÃO ALEATÓRIA DO VETOR
+    srand(time(NULL));
+    printf("VETOR: ");
+    for(int i=0; i<tamanho; i++){
+        vetor[i] = rand() % 10;
+        printf("%d ", vetor[i]);
     }
     
-    // BUSCA NÚMERO NÃO EXISTENTE BINÁRIA RECURSIVA LISTA2
-    printf("\nQual valor não existente deseja buscar? ");
-    scanf("%d", &busca);
-    
-    pos = busca_binaria_recursiva(lista2, busca, c, 0, lista2->quant-1);
-    if (pos != -1){
-        printf("COMPARAÇÕES: %d    BUSCA BINÁRIA RECURSIVA: %d\n", *c, pos);
-    } else {
-        printf("COMPARAÇÕES: %d    BUSCA BINÁRIA RECURSIVA: Não encontrado.\n", *c);
-    }    
-        // INSERINDO UM NÚMERO ALEATÓRIO DE FORMA ORDENADA
-    int n = rand()%100;
-    inserir_elemento_ordenado(lista2, n);
-    printf("\nElemento %d inserido:", n);
-    imprimir_elementos_recursivo(lista2, 0);
+    //TESTE DAS FUNÇÕES
+    printf("\n\nMÁXIMO: %d", maximo(vetor, tamanho));
+    printf("\nMÍNIMO: %d", minimo(vetor, tamanho));
+    printf("\nSOMA: %d", soma(vetor, tamanho));
+    printf("\nPRODUTO: %d", produto(vetor, tamanho));
 }
